5e9ae07f3a9355903113ee132820e2814fb6f076
trevinteacutter <trevin.teacutter@cerner.com>
2017-02-16 16:05:52 -0600

    Adding some unit tests for our DCOS components.

---
+++ app/scripts/modules/dcos/serverGroup/configure/CommandBuilder.spec.js
@@ -0,0 +1,57 @@
+'use strict';
+
+describe('dcosServerGroupCommandBuilder', function() {
+
+  beforeEach(
+    window.module(
+      require('./CommandBuilder.js')
+    )
+  );
+
+  beforeEach(window.inject(function(dcosServerGroupCommandBuilder, accountService, namingService, $q, $rootScope, _settings_) {
+    this.dcosServerGroupCommandBuilder = dcosServerGroupCommandBuilder;
+    this.$scope = $rootScope;
+    this.$q = $q;
+    this.settings = _settings_;
+    this.accountService = accountService;
+    this.namingService = namingService;
+  }));
+
+  describe('buildNewServerGroupCommand', function() {
+    it('should initializes to default values', function () {
+      var command = null;
+      this.dcosServerGroupCommandBuilder.buildNewServerGroupCommand({ name: 'dcosApp' }).then(function(result) {
+        command = result;
+      });
+
+      this.$scope.$digest();
+      expect(command.viewState.mode).toBe('create');
+    });
+  });
+
+  describe('buildServerGroupCommandFromExisting', function () {
+    it('should use base server group otherwise use the default', function() {
+      spyOn(this.namingService, 'parseServerGroupName').and.returnValue(this.$q.when('dcosApp-test-test'));
+
+      var baseServerGroup = {
+        account: 'prod',
+        region: 'test',
+        cluster: 'dcos-test-test',
+        type: 'dcos',
+        cloudProvider: 'dcos',
+        resources: {},
+        capacity: {},
+        image: {}
+      };
+
+      var command = null;
+      this.dcosServerGroupCommandBuilder.buildServerGroupCommandFromExisting({name: 'dcosApp'}, baseServerGroup).then(function(result) {
+        command = result;
+      });
+
+      this.$scope.$digest();
+
+      expect(command.viewState.mode).toBe('clone');
+    });
+  });
+});
